51755e7842395e1db86708d6ff1be7b0d91d9a3b
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-09-28 10:39:35 -0400

    Create MobLootPoolBuilder.java
    (cherry picked from commit e4bf77dc67b4f948e98b25ab10cb309a9129a31a)

---
+++ src/main/java/fzzyhmstrs/emi_loot/server/MobLootPoolBuilder.java
@@ -0,0 +1,42 @@
+package fzzyhmstrs.emi_loot.server;
+
+import fzzyhmstrs.emi_loot.util.TextKey;
+import net.minecraft.item.ItemStack;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MobLootPoolBuilder implements LootBuilder {
+
+    public MobLootPoolBuilder(float rollWeight, List<LootTableParser.LootConditionResult> conditions, List<LootTableParser.LootFunctionResult> functions){
+        this.rollWeight = rollWeight;
+        this.conditions = conditions;
+        this.functions = functions;
+    }
+
+    private final HashMap<List<TextKey>, ChestLootPoolBuilder> map = new HashMap<>();
+    private final float rollWeight;
+    final List<LootTableParser.LootConditionResult> conditions;
+    final List<LootTableParser.LootFunctionResult> functions;
+    HashMap<List<TextKey>, ChestLootPoolBuilder> builtMap = new HashMap<>();
+
+    public void addItem(LootTableParser.ItemEntryResult result){
+        List<TextKey> testKey = new LinkedList<>();
+        testKey.addAll(result.functions());
+        testKey.addAll(result.conditions());
+        ChestLootPoolBuilder builder = map.getOrDefault(testKey,new ChestLootPoolBuilder(rollWeight));
+        builder.addItem(result.item(),result.weight());
+        map.put(testKey,builder);
+    }
+
+    @Override
+    public void build() {
+        for (List<TextKey> key: map.keySet()){
+            ChestLootPoolBuilder builder = map.getOrDefault(key,new ChestLootPoolBuilder(rollWeight));
+            builder.build();
+            builtMap.put(key,builder);
+        }
+    }
+}
