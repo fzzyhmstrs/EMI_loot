b74cb01451bb988951946db0d64bc0601642bab6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-09-28 10:19:06 -0400

    Update EmiClientPlugin.java
    (cherry picked from commit d2cae09e31a1bdad40f5985793550b286343b32b)

--- src/main/java/fzzyhmstrs/emi_loot/emi/EmiClientPlugin.java
+++ src/main/java/fzzyhmstrs/emi_loot/emi/EmiClientPlugin.java
@@ -8,6 +8,7 @@ import fzzyhmstrs.emi_loot.EMILoot;
 import fzzyhmstrs.emi_loot.EMILootClient;
 import fzzyhmstrs.emi_loot.client.ClientBlockLootTable;
 import fzzyhmstrs.emi_loot.client.ClientChestLootTable;
+import fzzyhmstrs.emi_loot.client.ClientMobLootTable;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,14 +16,17 @@ import net.minecraft.util.registry.Registry;
 public class EmiClientPlugin implements EmiPlugin {
     private static final Identifier LOOT_ID = new Identifier(EMILoot.MOD_ID,"chest_loot");
     private static final Identifier BLOCK_ID = new Identifier(EMILoot.MOD_ID,"block_drops");
+    private static final Identifier MOB_ID = new Identifier(EMILoot.MOD_ID,"mob_drops");
     public static final EmiRecipeCategory LOOT_CATEGORY = new EmiRecipeCategory(LOOT_ID, EmiStack.of(Blocks.CHEST.asItem()), new LootSimplifiedRenderer(0,0));
     public static final EmiRecipeCategory BLOCK_CATEGORY = new EmiRecipeCategory(BLOCK_ID, EmiStack.of(Blocks.DIAMOND_ORE.asItem()), new LootSimplifiedRenderer(16,0));
+    public static final EmiRecipeCategory MOB_CATEGORY = new EmiRecipeCategory(MOB_ID, EmiStack.of(Blocks.WITHER_SKELETON_SKULL.asItem()), new LootSimplifiedRenderer(16,16));
 
     @Override
     public void register(EmiRegistry registry) {
 
         registry.addCategory(LOOT_CATEGORY);
         registry.addCategory(BLOCK_CATEGORY);
+        registry.addCategory(MOB_CATEGORY);
 
         EMILootClient.tables.getLoots().forEach(lootReceiver -> {
             if (lootReceiver instanceof ClientChestLootTable){
@@ -31,6 +35,11 @@ public class EmiClientPlugin implements EmiPlugin {
             if (lootReceiver instanceof ClientBlockLootTable){
                 if (!((ClientBlockLootTable) lootReceiver).isEmpty()) {
                     registry.addRecipe(new BlockLootRecipe((ClientBlockLootTable) lootReceiver));
+                }
+            }
+            if (lootReceiver instanceof ClientMobLootTable){
+                if (!((ClientMobLootTable) lootReceiver).isEmpty()) {
+                    registry.addRecipe(new MobLootRecipe((ClientMobLootTable) lootReceiver));
                 }
             }
         });
