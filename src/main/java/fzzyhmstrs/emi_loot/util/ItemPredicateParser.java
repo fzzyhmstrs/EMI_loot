8da3eabab33830d1b6ce7580f69af47978f74486
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-15 12:12:22 -0500

    Update ItemPredicateParser with LText

--- src/main/java/fzzyhmstrs/emi_loot/util/ItemPredicateParser.java
+++ src/main/java/fzzyhmstrs/emi_loot/util/ItemPredicateParser.java
@@ -21,31 +21,31 @@ public class ItemPredicateParser {
     public static Text parseItemPredicate(ItemPredicate predicate){
         TagKey<Item> tag = ((ItemPredicateAccessor)predicate).getTag();
         if (tag != null){
-            return Text.translatable("emi_loot.item_predicate.tag",tag.id());
+            return LText.translatable("emi_loot.item_predicate.tag",tag.id());
         }
         Set<Item> items = ((ItemPredicateAccessor)predicate).getItems();
         if (items != null){
             int size = items.size();
             if (size == 1){
-                return Text.translatable("emi_loot.item_predicate.items",items.stream().toList().get(0).getName());
+                return LText.translatable("emi_loot.item_predicate.items",items.stream().toList().get(0).getName());
             }
             if (size == 2){
                 List<Item> list = items.stream().toList();
-                return Text.translatable("emi_loot.item_predicate.items_2",list.get(0).getName(),list.get(1).getName());
+                return LText.translatable("emi_loot.item_predicate.items_2",list.get(0).getName(),list.get(1).getName());
             }
             if (size > 2){
                 List<Item> list = items.stream().toList();
-                MutableText finalText = Text.empty();
+                MutableText finalText = LText.empty();
                 for (int i = 0; i < size; i++){
                     Item item = list.get(i);
                     if (i == 0){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.items_3a", item.getName().getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.items_3a", item.getName().getString()));
                     } else if (i == size - 2){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.items_3c", item.getName().getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.items_3c", item.getName().getString()));
                     } else if (i == size - 1){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.items_3d", item.getName().getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.items_3d", item.getName().getString()));
                     } else {
-                        finalText.append(Text.translatable("emi_loot.item_predicate.items_3b", item.getName().getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.items_3b", item.getName().getString()));
                     }
                 }
                 return finalText;
@@ -57,7 +57,7 @@ public class ItemPredicateParser {
             Integer min = count.getMin();
             int finalMax = max != null ? max : 0;
             int finalMin = min != null ? min : 0;
-            return Text.translatable("emi_loot.item_predicate.count", Integer.toString(finalMin), Integer.toString(finalMax));
+            return LText.translatable("emi_loot.item_predicate.count", Integer.toString(finalMin), Integer.toString(finalMax));
         }
         NumberRange.IntRange durability = ((ItemPredicateAccessor)predicate).getDurability();
         if (durability != NumberRange.IntRange.ANY){
@@ -65,7 +65,7 @@ public class ItemPredicateParser {
             Integer min = durability.getMin();
             int finalMax = max != null ? max : 0;
             int finalMin = min != null ? min : 0;
-            return Text.translatable("emi_loot.item_predicate.durability", Integer.toString(finalMin), Integer.toString(finalMax));
+            return LText.translatable("emi_loot.item_predicate.durability", Integer.toString(finalMin), Integer.toString(finalMax));
         }
         EnchantmentPredicate[] enchants = ((ItemPredicateAccessor)predicate).getEnchantments();
         EnchantmentPredicate[] storedEnchants = ((ItemPredicateAccessor)predicate).getStoredEnchantments();
@@ -76,31 +76,31 @@ public class ItemPredicateParser {
             int size = list.size();
             if (size == 1){
                 Enchantment enchant = ((EnchantmentPredicateAccessor)list.get(0)).getEnchantment();
-                return Text.translatable("emi_loot.item_predicate.enchant",enchant.getName(1).getString());
+                return LText.translatable("emi_loot.item_predicate.enchant",enchant.getName(1).getString());
             }
             if (size == 2){
                 Enchantment enchant1 = ((EnchantmentPredicateAccessor)list.get(0)).getEnchantment();
                 Enchantment enchant2 = ((EnchantmentPredicateAccessor)list.get(1)).getEnchantment();
-                return Text.translatable("emi_loot.item_predicate.enchant_2",enchant1.getName(1).getString(),enchant2.getName(1).getString());
+                return LText.translatable("emi_loot.item_predicate.enchant_2",enchant1.getName(1).getString(),enchant2.getName(1).getString());
             }
             if (size > 2){
-                MutableText finalText = Text.empty();
+                MutableText finalText = LText.empty();
                 for (int i = 0; i < size; i++){
                     Enchantment enchant = ((EnchantmentPredicateAccessor)list.get(i)).getEnchantment();
                     if (i == 0){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.enchant_3a", enchant.getName(1).getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3a", enchant.getName(1).getString()));
                     } else if (i == size - 2){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.enchant_3c", enchant.getName(1).getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3c", enchant.getName(1).getString()));
                     } else if (i == size - 1){
-                        finalText.append(Text.translatable("emi_loot.item_predicate.enchant_3d", enchant.getName(1).getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3d", enchant.getName(1).getString()));
                     } else {
-                        finalText.append(Text.translatable("emi_loot.item_predicate.enchant_3b",enchant.getName(1).getString()));
+                        finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3b",enchant.getName(1).getString()));
                     }
                 }
                 return finalText;
             }
         }
-        return Text.translatable("emi_loot.item_predicate.empty");
+        return LText.translatable("emi_loot.item_predicate.empty");
     }
 
 }
