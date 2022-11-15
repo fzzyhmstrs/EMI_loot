af49086f808d8de4bcef7d7ec7fb3aa357f21fd1
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-15 11:01:07 -0500

    Adding my new LText

--- src/main/java/fzzyhmstrs/emi_loot/server/LootTableParser.java
+++ src/main/java/fzzyhmstrs/emi_loot/server/LootTableParser.java
@@ -5,6 +5,7 @@ import fzzyhmstrs.emi_loot.mixins.*;
 import fzzyhmstrs.emi_loot.util.DamageSourcePredicateParser;
 import fzzyhmstrs.emi_loot.util.ItemPredicateParser;
 import fzzyhmstrs.emi_loot.util.TextKey;
+import fzzyhmstrs.emi_loot.util.LText;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -303,7 +304,7 @@ public class LootTableParser {
         } else if (type == LootFunctionTypes.SET_POTION){
             Potion potion = ((SetPotionLootFunctionAccessor)function).getPotion();
             PotionUtil.setPotion(stack, potion);
-            Text potionName = Text.translatable(potion.finishTranslationKey(Items.POTION.getTranslationKey() + ".effect."));
+            Text potionName = LText.translatable(potion.finishTranslationKey(Items.POTION.getTranslationKey() + ".effect."));
             return new LootFunctionResult(TextKey.of("emi_loot.function.potion",potionName.getString()), ItemStack.EMPTY);
         } else if (type == LootFunctionTypes.SET_COUNT){
             LootNumberProvider provider = ((SetCountLootFunctionAccessor)function).getCountRange();
@@ -380,7 +381,7 @@ public class LootTableParser {
                 MapState.addDecorationsNbt(mapStack, BlockPos.ORIGIN,"+",decoration);
                 typeKey = "emi_loot.map."+ destination.id().getPath();
             }
-            return new LootFunctionResult(TextKey.of("emi_loot.function.map",Text.translatable(typeKey).getString()), mapStack);
+            return new LootFunctionResult(TextKey.of("emi_loot.function.map",LText.translatable(typeKey).getString()), mapStack);
         } else if (type == LootFunctionTypes.SET_NAME){
             Text text = ((SetNameLootFunctionAccessor)function).getName();
             stack.setCustomName(text);
@@ -507,7 +508,7 @@ public class LootTableParser {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private static Text compileConditionTexts(ItemStack stack,List<LootConditionResult> results){
-        MutableText finalText = Text.empty();
+        MutableText finalText = LText.empty();
         int size = results.size();
         for(int i = 0; i < size;i++){
             LootConditionResult result = results.get(i);
@@ -518,7 +519,7 @@ public class LootTableParser {
                 finalText.append(resultText);
             }
             if (i<(size - 1)){
-                finalText.append(Text.translatable("emi_loot.and"));
+                finalText.append(LText.translatable("emi_loot.and"));
             }
         }
         return finalText;
