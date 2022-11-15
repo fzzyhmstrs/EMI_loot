2fde01042e98558529af052a59771c33b78bb40d
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-15 08:46:46 -0500

    Create LText.java

---
+++ src/main/java/fzzyhmstrs/emi_loot/util/LText.java
@@ -0,0 +1,24 @@
+package fzzyhmstrs.emi_loot.util;
+
+import net.minecraft.text.Text;
+
+
+public class LText{
+
+    public static MutableText translatable(String key){
+        return Text.translatable(key)
+    }
+    
+    public static MutableText translatable(String key, Object[] args){
+        return Text.translatable(key, args)
+    }
+
+    public static MutableText literal(String msg){
+        return Text.literal(msg)
+    }
+    
+    public static MutableText empty(){
+        return Text.empty()    
+    }
+
+}
