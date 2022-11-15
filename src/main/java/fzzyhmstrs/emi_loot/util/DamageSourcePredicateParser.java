64480ac443282521b01000c9e3914b315fd0e56f
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-15 12:14:23 -0500

    Update DamageSourcePredicate with LText

--- src/main/java/fzzyhmstrs/emi_loot/util/DamageSourcePredicateParser.java
+++ src/main/java/fzzyhmstrs/emi_loot/util/DamageSourcePredicateParser.java
@@ -11,89 +11,89 @@ public class DamageSourcePredicateParser {
 
         EntityPredicate sourcePredicate = ((DamageSourcePredicateAccessor)predicate).getSourceEntity();
         Text sourceText = EntityPredicateParser.parseEntityPredicate(sourcePredicate);
-        if (!sourceText.equals(Text.empty())){
+        if (!sourceText.equals(LText.empty())){
             return sourceText;
         }
 
         Boolean isProjectile = ((DamageSourcePredicateAccessor)predicate).getIsProjectile();
         if (isProjectile != null){
             if (isProjectile){
-                return Text.translatable("emi_loot.damage_predicate.projectile_true");
+                return LText.translatable("emi_loot.damage_predicate.projectile_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.projectile_false");
+                return LText.translatable("emi_loot.damage_predicate.projectile_false");
             }
         }
 
         Boolean isExplosion = ((DamageSourcePredicateAccessor)predicate).getIsExplosion();
         if (isExplosion != null){
             if (isExplosion){
-                return Text.translatable("emi_loot.damage_predicate.explosion_true");
+                return LText.translatable("emi_loot.damage_predicate.explosion_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.explosion_false");
+                return LText.translatable("emi_loot.damage_predicate.explosion_false");
             }
         }
 
         Boolean bypassesArmor = ((DamageSourcePredicateAccessor)predicate).getBypassesArmor();
         if (bypassesArmor != null){
             if (bypassesArmor){
-                return Text.translatable("emi_loot.damage_predicate.bypass_armor_true");
+                return LText.translatable("emi_loot.damage_predicate.bypass_armor_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.bypass_armor_false");
+                return LText.translatable("emi_loot.damage_predicate.bypass_armor_false");
             }
         }
 
         Boolean bypassesInvulnerability = ((DamageSourcePredicateAccessor)predicate).getBypassesInvulnerability();
         if (bypassesInvulnerability != null){
             if (bypassesInvulnerability){
-                return Text.translatable("emi_loot.damage_predicate.no_invulnerable_true");
+                return LText.translatable("emi_loot.damage_predicate.no_invulnerable_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.no_invulnerable_false");
+                return LText.translatable("emi_loot.damage_predicate.no_invulnerable_false");
             }
         }
 
         Boolean bypassesMagic = ((DamageSourcePredicateAccessor)predicate).getBypassesMagic();
         if (bypassesMagic != null){
             if (bypassesMagic){
-                return Text.translatable("emi_loot.damage_predicate.unblockable_true");
+                return LText.translatable("emi_loot.damage_predicate.unblockable_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.unblockable_false");
+                return LText.translatable("emi_loot.damage_predicate.unblockable_false");
             }
         }
 
         Boolean isFire = ((DamageSourcePredicateAccessor)predicate).getIsFire();
         if (isFire != null){
             if (isFire){
-                return Text.translatable("emi_loot.damage_predicate.fire_true");
+                return LText.translatable("emi_loot.damage_predicate.fire_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.fire_false");
+                return LText.translatable("emi_loot.damage_predicate.fire_false");
             }
         }
 
         Boolean isMagic = ((DamageSourcePredicateAccessor)predicate).getIsMagic();
         if (isMagic != null){
             if (isMagic){
-                return Text.translatable("emi_loot.damage_predicate.magic_true");
+                return LText.translatable("emi_loot.damage_predicate.magic_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.magic_false");
+                return LText.translatable("emi_loot.damage_predicate.magic_false");
             }
         }
 
         Boolean isLightning = ((DamageSourcePredicateAccessor)predicate).getIsLightning();
         if (isLightning != null){
             if (isLightning){
-                return Text.translatable("emi_loot.damage_predicate.lightning_true");
+                return LText.translatable("emi_loot.damage_predicate.lightning_true");
             } else {
-                return Text.translatable("emi_loot.damage_predicate.lightning_false");
+                return LText.translatable("emi_loot.damage_predicate.lightning_false");
             }
         }
 
         EntityPredicate directPredicate = ((DamageSourcePredicateAccessor)predicate).getDirectEntity();
         Text directText = EntityPredicateParser.parseEntityPredicate(directPredicate);
-        if (!directText.equals(Text.empty())){
+        if (!directText.equals(LText.empty())){
             return directText;
         }
 
-        return Text.empty();
+        return LText.empty();
     }
 
 }
