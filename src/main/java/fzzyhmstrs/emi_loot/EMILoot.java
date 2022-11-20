5c3ecd469aa9d2dbb7ab3b0b4f4b8eea6aed34fa
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-11-20 09:52:49 -0500

    added DEBUG flag for printlning conditionally

--- src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
+++ src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class EMILoot implements ModInitializer {
 
+    public static boolean DEBUG = false;
     public static String MOD_ID = "emi_loot";
     public static Random emiLootRandom = new Random(System.currentTimeMillis());
     public static LootTableParser parser = new LootTableParser();
