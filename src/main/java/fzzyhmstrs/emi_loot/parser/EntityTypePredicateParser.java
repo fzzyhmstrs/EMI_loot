f27a7511d8266ec1cc7f58d42cdd996e0bdc337d
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-30 07:48:44 -0500

    Create EntityTypePredicateParser.java

---
+++ src/main/java/fzzyhmstrs/emi_loot/parser/EntityTypePredicateParser.java
@@ -0,0 +1,21 @@
+package fzzyhmstrs.emi_loot.parser;
+
+import fzzyhmstrs.emi_loot.util.LText;
+import net.minecraft.entity.EntityType;
+import net.minecraft.predicate.entity.*;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class EntityTypePredicateParser{
+
+    public static Text parseEntityTypePredicate(EntityTypePredicate predicate){
+         String jsonString = typePredicate.toJson().getAsString();
+        if (jsonString.startsWith("#")) {
+            return LText.translatable("emi_loot.entity_predicate.type_tag", jsonString);
+        } else {
+            EntityType<?> type = Registry.ENTITY_TYPE.get(new Identifier(jsonString));
+            return LText.translatable("emi_loot.mob_type_predicate.type", type.getName().getString());
+        }
+    }
+}
