6d821c90df269fff45e96668d7e1a8cc6b9be0ba
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-30 12:50:57 -0500

    Add basic config to the EMILoot main class.
    Will likely add some synchronizing code later so the server can update the client as needed. Or will integrate something into the sender packets to notify the client about a skipped thing and not to register that.
    
    (cherry picked from commit ab8744f5274e57a75ceb49dde9ed580f2d464c8c)

--- src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
+++ src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
@@ -17,6 +17,8 @@ public class EMILoot implements ModInitializer {
     public static String MOD_ID = "emi_loot";
     public static Random emiLootRandom = new Random(System.currentTimeMillis());
     public static LootTableParser parser = new LootTableParser();
+    private static Gson gson = GsonBuilder().create();
+    public static EmiLootConfig config = readOrCreate();
     public static Enchantment RANDOM = new Enchantment(Enchantment.Rarity.VERY_RARE, EnchantmentTarget.TRIDENT, EquipmentSlot.values()){
         @Override
         public boolean isAvailableForEnchantedBookOffer() {
@@ -32,5 +34,40 @@ public class EMILoot implements ModInitializer {
     public void onInitialize() {
         parser.registerServer();
         Registry.register(Registry.ENCHANTMENT,new Identifier(MOD_ID,"random"),RANDOM);
+    }
+    
+    private static EmiLootConfig readOrCreate(){
+        File dir = FabricLoader.getInstance().configDir.toFile();
+        
+        if (!dir.exists() && !dir.mkdirs()) {
+            System.out.println("EMI Loot could not find or create config directory, using default configs");
+            return new EmiLootConfig();
+        }
+        
+        File f = new File(dir,"EmiLootConfig.json");
+        
+        try{
+            if (f.exists()){
+                return gson.fromJson(f.readLines().joinToString(""),EmiLootConfig.class);
+            } else if (!f.createNewFile()){
+                throw new UnsupportedOperationException("couldn't generate config file");
+            } else {
+                f.writeText(gson.toJson(new EmiLootConfig()));
+            }
+        } catch(Exception e){
+            System.out.println("Emi Loot failed to create or read it's config file!);
+            System.out.println(e.getStackTrace());
+            return EmiLootConfig();
+        }
+    }
+    
+    public static class EmiLootConfig{
+        public boolean parseChestLoot = true;
+        
+        public boolean parseBlockLoot = true;
+        
+        public boolean parseMobLoot = true;
+    
+        public boolean parseGameplayLoot = true;
     }
 }
