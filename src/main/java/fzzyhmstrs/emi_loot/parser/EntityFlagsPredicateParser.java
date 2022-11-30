5c3b99049050a0c02a71ae2856704bac4feaa059
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-11-30 08:19:29 -0500

    Create EntityFlagsPredicateParser.java

---
+++ src/main/java/fzzyhmstrs/emi_loot/parser/EntityFlagsPredicateParser.java
@@ -0,0 +1,58 @@
+package fzzyhmstrs.emi_loot.parser;
+
+import fzzyhmstrs.emi_loot.mixins.*;
+import fzzyhmstrs.emi_loot.util.LText;
+import net.minecraft.predicate.entity.*;
+import net.minecraft.text.Text;
+
+public class EntityFlagsPredicateParser{
+
+    public static Text parseEntityFlagsPredicate(EntityFlagsPredicate predicate){
+        Boolean isOnFire = ((EntityFlagsPredicateAccessor)entityFlagsPredicate).getIsOnFire();
+        if (isOnFire != null){
+            if (isOnFire){
+                return LText.translatable("emi_loot.entity_predicate.fire_true");
+            } else {
+                return LText.translatable("emi_loot.entity_predicate.fire_false");
+            }
+        }
+
+        Boolean isSneaking = ((EntityFlagsPredicateAccessor)entityFlagsPredicate).getIsSneaking();
+        if (isSneaking != null){
+            if (isSneaking){
+                return LText.translatable("emi_loot.entity_predicate.sneak_true");
+            } else {
+                return LText.translatable("emi_loot.entity_predicate.sneak_false");
+            }
+        }
+
+        Boolean isSprinting = ((EntityFlagsPredicateAccessor)entityFlagsPredicate).getIsSprinting();
+        if (isSprinting != null){
+            if (isSprinting){
+                return LText.translatable("emi_loot.entity_predicate.sprint_true");
+            } else {
+                return LText.translatable("emi_loot.entity_predicate.sprint_false");
+            }
+        }
+
+        Boolean isSwimming = ((EntityFlagsPredicateAccessor)entityFlagsPredicate).getIsSwimming();
+        if (isSwimming != null){
+            if (isSwimming){
+                return LText.translatable("emi_loot.entity_predicate.swim_true");
+            } else {
+                return LText.translatable("emi_loot.entity_predicate.swim_false");
+            }
+        }
+
+        Boolean isBaby = ((EntityFlagsPredicateAccessor)entityFlagsPredicate).getIsBaby();
+        if (isBaby != null){
+            if (isBaby){
+                return LText.translatable("emi_loot.entity_predicate.baby_true");
+            } else {
+                return LText.translatable("emi_loot.entity_predicate.baby_false");
+            }
+        }
+        return LText.empty();
+    }
+
+}
