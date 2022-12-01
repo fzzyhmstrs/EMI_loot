87f4e266ecca33e1362ea5e9c993666218099dff
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-01 11:57:05 -0500

    Update StatePredicateParser.java

--- src/main/java/fzzyhmstrs/emi_loot/parser/StatePredicateParser.java
+++ src/main/java/fzzyhmstrs/emi_loot/parser/StatePredicateParser.java
@@ -1,4 +1,11 @@
 package fzzyhmstrs.emi_loot.parser;
 
+
+
 public class StatePredicateParser {
+  
+    public static Text parseStatePredicate(StatePredicate predicate){
+        return LText.empty();
+    }
+  
 }
