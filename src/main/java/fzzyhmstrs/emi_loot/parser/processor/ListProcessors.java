c87977c7b6cf9d2abc7a166b240f0c754cf4b552
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-01 10:34:43 -0500

    Update ListProcessors.java
    (cherry picked from commit 28035c397eb14be19dfb14a1c155c29c3efa3094)

--- src/main/java/fzzyhmstrs/emi_loot/parser/processor/ListProcessors.java
+++ src/main/java/fzzyhmstrs/emi_loot/parser/processor/ListProcessors.java
@@ -7,14 +7,32 @@ import net.minecraft.text.Text;
 import java.util.List;
 
 public class ListProcessors {
+    
+    public static Text buildAndList(List<MutableText> list){
+        return buildAndList(list, 0);
+    }
 
-    public static Text buildList(List<MutableText> list, int currentIndex,String list2Key, String list3Key){
+    public static Text buildAndList(List<MutableText> list, int currentIndex){
         if (currentIndex == (list.size() - 1)){
             return list.get(currentIndex);
         } else if (currentIndex == (list.size() - 2)){
-            return LText.translatable(list2Key,list.get(currentIndex),buildList(list, currentIndex + 1, list2Key, list3Key).getString());
+            return LText.translatable("emi_loot.predicate.and_2",list.get(currentIndex),buildList(list, currentIndex + 1, "emi_loot.predicate.and_2", "emi_loot.predicate.and_3").getString());
         } else {
-            return LText.translatable(list3Key,list.get(currentIndex),buildList(list, currentIndex + 1, list2Key, list3Key).getString());
+            return LText.translatable("emi_loot.predicate.and_3",list.get(currentIndex),buildList(list, currentIndex + 1, "emi_loot.predicate.and_2", "emi_loot.predicate.and_3").getString());
+        }
+    }
+    
+    public static Text buildOrList(List<MutableText> list){
+        return buildOrList(list, 0);
+    }
+    
+    public static Text buildOrList(List<MutableText> list, int currentIndex){
+        if (currentIndex == (list.size() - 1)){
+            return list.get(currentIndex);
+        } else if (currentIndex == (list.size() - 2)){
+            return LText.translatable("emi_loot.predicate.or_2",list.get(currentIndex),buildList(list, currentIndex + 1, "emi_loot.predicate.or_2", "emi_loot.predicate.or_3").getString());
+        } else {
+            return LText.translatable("emi_loot.predicate.or_3",list.get(currentIndex),buildList(list, currentIndex + 1, "emi_loot.predicate.or_2", "emi_loot.predicate.or_3").getString());
         }
     }
 
