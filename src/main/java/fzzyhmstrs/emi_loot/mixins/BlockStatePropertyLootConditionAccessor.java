58b1666e496398b29e2afcafa66e16565cdccf92
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-01 11:48:23 -0500

    Create BlockStatePropertyLootConditionAccessor.java

---
+++ src/main/java/fzzyhmstrs/emi_loot/mixins/BlockStatePropertyLootConditionAccessor.java
@@ -0,0 +1,19 @@
+package fzzyhmstrs.emi_loot.mixins;
+
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.block.Block;
+import net.minecraft.predicate.StatePredicate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BlockStatePropertyLootCondition.class)
+public interface BlockStatePropertyLootConditionAccessor{
+
+  @Accessor(value = "block")
+  Block getBlock();
+
+  @Accessor(value = "properties")
+  StatePredicate getProperties();
+
+}
