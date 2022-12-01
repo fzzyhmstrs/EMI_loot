d4b833b61bc2b487b8d0318ae02569a3680fd333
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-01 11:17:25 -0500

    Create EntityPropertiesLootConditionAccessor.java
    (cherry picked from commit 927dd22bc2ac699903fef15d8b3b786d5fc49ed2)

---
+++ src/main/java/fzzyhmstrs/emi_loot/mixins/EntityPropertiesLootConditionAccessor.java
@@ -0,0 +1,16 @@
+package fzzyhmstrs.emi_loot.mixins;
+
+import net.minecraft.loot.condition.EntityPropertiesLootCondition;
+import net.minecraft.loot.context.LootContext.EntityTarget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityPropertiesLootCondition.class)
+public interface EntityPropertiesLootConditionAccessor{
+
+    @Accessor(value = "entity")
+    LootContext.EntityTarget getEntity();
+    
+    @Accessor(value = "predicate")
+    EntityPredicate getPredicate();
+}
