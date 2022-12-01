09f503699c4b0009f6d0e4c144e1f4b07fa5b30c
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-01 10:25:37 -0500

    Update EnchantmentPredicateParser.java

--- src/main/java/fzzyhmstrs/emi_loot/parser/EnchantmentPredicateParser.java
+++ src/main/java/fzzyhmstrs/emi_loot/parser/EnchantmentPredicateParser.java
@@ -1,6 +1,7 @@
 package fzzyhmstrs.emi_loot.parser;
 
 import fzzyhmstrs.emi_loot.mixins.EnchantmentPredicateAccessor;
+import fzzyhmstrs.emi_loot.parser.processor.ListProcessors;
 import fzzyhmstrs.emi_loot.util.LText;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.predicate.item.EnchantmentPredicate;
@@ -8,35 +9,18 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
 import java.util.List;
+import java.util.LinkedList;
 
 public class EnchantmentPredicateParser{
 
     public static Text parseEnchantmentPredicates(List<EnchantmentPredicate> list){
-        int size = list.size();
-        if (size == 1){
-            Enchantment enchant = ((EnchantmentPredicateAccessor)list.get(0)).getEnchantment();
-            return LText.translatable("emi_loot.item_predicate.enchant",enchant.getName(1).getString());
+        List<MutableText> list2 = new LinkedList<>();
+        for (EnchantmentPredicate predicate : list){
+            Enchantment enchant = ((EnchantmentPredicateAccessor)predicate).getEnchantment();
+            list2.add(enchant.getName(1));
         }
-        if (size == 2){
-            Enchantment enchant1 = ((EnchantmentPredicateAccessor)list.get(0)).getEnchantment();
-            Enchantment enchant2 = ((EnchantmentPredicateAccessor)list.get(1)).getEnchantment();
-            return LText.translatable("emi_loot.item_predicate.enchant_2",enchant1.getName(1).getString(),enchant2.getName(1).getString());
-        }
-        if (size > 2){
-            MutableText finalText = LText.empty();
-            for (int i = 0; i < size; i++){
-                Enchantment enchant = ((EnchantmentPredicateAccessor)list.get(i)).getEnchantment();
-                if (i == 0){
-                    finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3a", enchant.getName(1).getString()));
-                } else if (i == size - 2){
-                    finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3c", enchant.getName(1).getString()));
-                } else if (i == size - 1){
-                    finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3d", enchant.getName(1).getString()));
-                } else {
-                    finalText.append(LText.translatable("emi_loot.item_predicate.enchant_3b",enchant.getName(1).getString()));
-                }
-            }
-            return finalText;
+        if (!list2.isEmpty()){
+            return LText.translatable("emi_loot.item_predicate.enchant", ListProcessors.buildAndList(list2));
         }
         return LText.empty();
     }
