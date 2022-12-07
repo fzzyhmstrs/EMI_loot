1ae3941dee5a8483ff410be2dbcb6d998d47520c
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-07 11:42:14 -0500

    Create FunctionParser.java
    (cherry picked from commit 2a0ff37006c68b66a5cbce4c74d5f9fb602ba09e)

---
+++ src/main/java/fzzyhmstrs/emi_loot/parser/function/FunctionParser.java
@@ -0,0 +1,17 @@
+package fzzyhmstrs.emi_loot.parser.condition;
+
+package fzzyhmstrs.emi_loot.parser.LootTableParser;
+import net.minecraft.loot.function.LootFunction;
+
+import java.util.List;
+
+public interface FunctionParser{
+    public static FunctionParser EMPTY = new FunctionParser(){
+        @Override
+        public LootTableParser.LootFunctionResult parseFunction(LootFunction function){
+            return LootFunctionResult.EMPTY;
+        }
+    }
+    
+    LootTableParser.LootFunctionResult parseFunction(LootFunction function);
+}
