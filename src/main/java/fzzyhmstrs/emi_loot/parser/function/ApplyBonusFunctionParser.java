f6d1f2f4b0e3d1a130db4848045cca02f4ef2e96
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-12-07 14:18:06 -0500

    Create ApplyBonusFunctionParser.java
    (cherry picked from commit da30baa001c8da3be76717cd6fe3e24ab1cc2106)

---
+++ src/main/java/fzzyhmstrs/emi_loot/parser/function/ApplyBonusFunctionParser.java
@@ -0,0 +1,23 @@
+package fzzyhmstrs.emi_loot.parser.condition;
+
+package fzzyhmstrs.emi_loot.parser.LootTableParser;
+import net.minecraft.loot.function.LootFunction;
+import net.minecraft.enchantment.Enchantment;
+
+import java.util.List;
+
+public class ApplyBonusFunctionParser{
+    
+    @Override
+    public LootTableParser.LootFunctionResult parseFunction(LootFunction function, List<TextKey> conditionTexts){
+        Enchantment enchant = ((ApplyBonusLootFunctionAccessor)function).getEnchantment();
+        String name = enchant.getName(1).getString();
+        String nTrim;
+        if (enchant.getMaxLevel() != 1) {
+            nTrim = name.substring(0, name.length() - 2);
+        } else {
+            nTrim = name;
+        }
+        return new LootFunctionResult(TextKey.of("emi_loot.function.bonus",nTrim),ItemStack.EMPTY,conditionsTexts);
+    }
+}
