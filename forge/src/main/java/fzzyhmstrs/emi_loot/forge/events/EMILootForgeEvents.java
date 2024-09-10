63bee92ae73a8c3b226ebf1b2ad77345e0e0ee06
unilock <unilock@fennet.rentals>
2024-09-10 11:51:50 -0400

    Forge: Add PlayerLoggedInEvent listener
    Signed-off-by: unilock <unilock@fennet.rentals>

--- forge/src/main/java/fzzyhmstrs/emi_loot/forge/events/EMILootForgeEvents.java
+++ forge/src/main/java/fzzyhmstrs/emi_loot/forge/events/EMILootForgeEvents.java
@@ -1,10 +1,19 @@
 package fzzyhmstrs.emi_loot.forge.events;
 
 import fzzyhmstrs.emi_loot.EMILoot;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraftforge.event.OnDatapackSyncEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EMILootForgeEvents {
+    @SubscribeEvent
+    public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity() instanceof ServerPlayerEntity player) {
+            EMILoot.parser.registerServer(player);
+        }
+    }
+
     @SubscribeEvent
     public void onDatapackSync(OnDatapackSyncEvent event) {
         if (event.getPlayer() == null) {
