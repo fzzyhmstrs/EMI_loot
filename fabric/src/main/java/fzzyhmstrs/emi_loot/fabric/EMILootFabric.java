b976704b7752915c4abe08ad0a3a044848600cce
unilock <unilock@fennet.rentals>
2024-09-10 13:12:25 -0400

    Remove unnecessary LootConditionTypes / LootFunctionTypes access widening
    Signed-off-by: unilock <unilock@fennet.rentals>

--- fabric/src/main/java/fzzyhmstrs/emi_loot/fabric/EMILootFabric.java
+++ fabric/src/main/java/fzzyhmstrs/emi_loot/fabric/EMILootFabric.java
@@ -8,17 +8,20 @@ import fzzyhmstrs.emi_loot.server.function.OminousBannerLootFunction;
 import fzzyhmstrs.emi_loot.server.function.SetAnyDamageLootFunction;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.loot.condition.LootConditionTypes;
-import net.minecraft.loot.function.LootFunctionTypes;
+import net.minecraft.loot.condition.LootConditionType;
+import net.minecraft.loot.function.LootFunctionType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class EMILootFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        EMILoot.WITHER_KILL = () -> LootConditionTypes.register("lootify:wither_kill", new KilledByWitherLootCondition.Serializer());
-        EMILoot.SPAWNS_WITH = () -> LootConditionTypes.register("lootify:spawns_with", new MobSpawnedWithLootCondition.Serializer());
-        EMILoot.CREEPER = () -> LootConditionTypes.register("lootify:creeper", new BlownUpByCreeperLootCondition.Serializer());
-        EMILoot.SET_ANY_DAMAGE = () -> LootFunctionTypes.register("lootify:set_any_damage", new SetAnyDamageLootFunction.Serializer());
-        EMILoot.OMINOUS_BANNER = () -> LootFunctionTypes.register("lootify:ominous_banner", new OminousBannerLootFunction.Serializer());
+        EMILoot.WITHER_KILL = () -> Registry.register(Registries.LOOT_CONDITION_TYPE, new Identifier("lootify", "wither_kill"), new LootConditionType(new KilledByWitherLootCondition.Serializer()));
+        EMILoot.SPAWNS_WITH = () -> Registry.register(Registries.LOOT_CONDITION_TYPE, new Identifier("lootify", "spawns_with"), new LootConditionType(new MobSpawnedWithLootCondition.Serializer()));
+        EMILoot.CREEPER = () -> Registry.register(Registries.LOOT_CONDITION_TYPE, new Identifier("lootify", "creeper"), new LootConditionType(new BlownUpByCreeperLootCondition.Serializer()));
+        EMILoot.SET_ANY_DAMAGE = () -> Registry.register(Registries.LOOT_FUNCTION_TYPE, new Identifier("lootify", "set_any_damage"), new LootFunctionType(new SetAnyDamageLootFunction.Serializer()));
+        EMILoot.OMINOUS_BANNER = () -> Registry.register(Registries.LOOT_FUNCTION_TYPE, new Identifier("lootify", "ominous_banner"), new LootFunctionType(new OminousBannerLootFunction.Serializer()));
 
         ServerLifecycleEvents.SYNC_DATA_PACK_CONTENTS.register((player, joined) ->{
             EMILoot.parser.registerServer(player);
