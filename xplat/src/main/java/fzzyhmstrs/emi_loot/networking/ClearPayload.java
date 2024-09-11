bb001c6a2f1a639136fd41778da8712f9bf11977
unilock <unilock@fennet.rentals>
2024-09-10 21:06:29 -0400

    Fix ClearPayload
    Signed-off-by: unilock <unilock@fennet.rentals>

--- xplat/src/main/java/fzzyhmstrs/emi_loot/networking/ClearPayload.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/networking/ClearPayload.java
@@ -4,11 +4,10 @@ import fzzyhmstrs.emi_loot.EMILoot;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.util.Identifier;
 
 public class ClearPayload implements CustomPayload {
 	public static final ClearPayload INSTANCE = new ClearPayload();
-	public static final Id<ClearPayload> TYPE = new CustomPayload.Id<>(Identifier.of(EMILoot.MOD_ID, "clear_loot"));
+	public static final Id<ClearPayload> TYPE = new CustomPayload.Id<>(EMILoot.identity("clear_loot"));
 	public static final PacketCodec<ByteBuf, ClearPayload> CODEC = PacketCodec.unit(INSTANCE);
 
 	@Override
