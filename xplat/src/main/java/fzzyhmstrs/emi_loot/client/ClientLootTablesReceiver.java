4051f3c5e7271f55fb5852663883b7e346d4ae53
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-12 17:49:26 -0400

    fix 1.20.4

--- xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientLootTablesReceiver.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/client/ClientLootTablesReceiver.java
@@ -1,32 +1,28 @@
 package fzzyhmstrs.emi_loot.client;
 
-import fzzyhmstrs.emi_loot.networking.ArchaeologyLootPayload;
-import fzzyhmstrs.emi_loot.networking.BlockLootPayload;
-import fzzyhmstrs.emi_loot.networking.ChestLootPayload;
-import fzzyhmstrs.emi_loot.networking.GameplayLootPayload;
-import fzzyhmstrs.emi_loot.networking.MobLootPayload;
+import fzzyhmstrs.emi_loot.util.SimpleCustomPayload;
 import me.fzzyhmstrs.fzzy_config.networking.api.ClientPlayNetworkContext;
 
 public class ClientLootTablesReceiver {
 
-	public static void receiveChestSender(ChestLootPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveChestSender(payload.buf(), ctx.player().getWorld());
+	public static void receiveChestSender(SimpleCustomPayload payload, ClientPlayNetworkContext ctx) {
+		ClientLootTables.INSTANCE.receiveChestSender(payload.buf());
 	}
 
-	public static void receiveBlockSender(BlockLootPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveBlockSender(payload.buf(), ctx.player().getWorld());
+	public static void receiveBlockSender(SimpleCustomPayload payload, ClientPlayNetworkContext ctx) {
+		ClientLootTables.INSTANCE.receiveBlockSender(payload.buf());
 	}
 
-	public static void receiveMobSender(MobLootPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveMobSender(payload.buf(), ctx.player().getWorld());
+	public static void receiveMobSender(SimpleCustomPayload payload, ClientPlayNetworkContext ctx) {
+		ClientLootTables.INSTANCE.receiveMobSender(payload.buf());
 	}
 
-	public static void receiveGameplaySender(GameplayLootPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveGameplaySender(payload.buf(), ctx.player().getWorld());
+	public static void receiveGameplaySender(SimpleCustomPayload payload, ClientPlayNetworkContext ctx) {
+		ClientLootTables.INSTANCE.receiveGameplaySender(payload.buf());
 	}
 
-	public static void receiveArchaeologySender(ArchaeologyLootPayload payload, ClientPlayNetworkContext ctx) {
-		ClientLootTables.INSTANCE.receiveArchaeologySender(payload.buf(), ctx.player().getWorld());
+	public static void receiveArchaeologySender(SimpleCustomPayload payload, ClientPlayNetworkContext ctx) {
+		ClientLootTables.INSTANCE.receiveArchaeologySender(payload.buf());
 	}
 
 }
