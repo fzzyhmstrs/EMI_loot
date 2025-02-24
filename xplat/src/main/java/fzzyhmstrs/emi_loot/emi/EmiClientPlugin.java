a8ea2c0c66b5dfd37934ce3521b31351b4d8c99b
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-24 15:25:20 -0500

    0.7.6

--- xplat/src/main/java/fzzyhmstrs/emi_loot/emi/EmiClientPlugin.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/emi/EmiClientPlugin.java
@@ -1,6 +1,7 @@
 package fzzyhmstrs.emi_loot.emi;
 
 import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiInitRegistry;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
@@ -12,9 +13,15 @@ import fzzyhmstrs.emi_loot.client.ClientChestLootTable;
 import fzzyhmstrs.emi_loot.client.ClientGameplayLootTable;
 import fzzyhmstrs.emi_loot.client.ClientLootTables;
 import fzzyhmstrs.emi_loot.client.ClientMobLootTable;
+import fzzyhmstrs.emi_loot.client.LootReceiver;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @EmiEntrypoint
 public class EmiClientPlugin implements EmiPlugin {
@@ -29,6 +36,101 @@ public class EmiClientPlugin implements EmiPlugin {
     public static final EmiRecipeCategory GAMEPLAY_CATEGORY = new EmiRecipeCategory(GAMEPLAY_ID, EmiStack.of(Items.FISHING_ROD), new LootSimplifiedRenderer(16, 16));
     public static final EmiRecipeCategory ARCHAEOLOGY_CATEGORY = new EmiRecipeCategory(ARCHAEOLOGY_ID, EmiStack.of(Items.BRUSH));
 
+    private static CompletableFuture<List<ChestLootRecipe>> chestFuture;
+    private static CompletableFuture<List<BlockLootRecipe>> blockFuture;
+    private static CompletableFuture<List<MobLootRecipe>> mobFuture;
+    private static CompletableFuture<List<GameplayLootRecipe>> gameplayFuture;
+    private static CompletableFuture<List<ArchaeologyLootRecipe>> archaeologyFuture;
+
+    @Override
+    public void initialize(EmiInitRegistry registry) {
+        chestFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return ClientLootTables.INSTANCE.chestLoots.stream().flatMap(lr -> {
+                    try {
+                        return Stream.of(new ChestLootRecipe((ClientChestLootTable) lr));
+                    } catch (Throwable e) {
+                        EMILoot.LOGGER.error("Critical error encountered while creating a Chest loot recipe; recipe skipped: {}", lr.getId().toString());
+                        EMILoot.LOGGER.error("Thrown Error: ", e);
+                        return Stream.of();
+                    }
+                }).toList();
+            } catch (Throwable e) {
+                EMILoot.LOGGER.error("Critical error encountered while creating all Chest loot recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }
+        });
+        blockFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return ClientLootTables.INSTANCE.blockLoots.stream().flatMap(lr -> {
+                    try {
+                        return Stream.of(new BlockLootRecipe((ClientBlockLootTable) lr));
+                    } catch (Throwable e) {
+                        EMILoot.LOGGER.error("Critical error encountered while creating a Block loot recipe; recipe skipped: {}", lr.getId().toString());
+                        EMILoot.LOGGER.error("Thrown Error: ", e);
+                        return Stream.of();
+                    }
+                }).toList();
+            } catch (Throwable e) {
+                EMILoot.LOGGER.error("Critical error encountered while creating all Block Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }
+        });
+        mobFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return ClientLootTables.INSTANCE.mobLoots.stream().flatMap(lr -> {
+                    try {
+                        return Stream.of(new MobLootRecipe((ClientMobLootTable) lr));
+                    } catch (Throwable e) {
+                        EMILoot.LOGGER.error("Critical error encountered while creating a Mob loot recipe; recipe skipped: {}", lr.getId().toString());
+                        EMILoot.LOGGER.error("Thrown Error: ", e);
+                        return Stream.of();
+                    }
+                }).toList();
+            } catch (Throwable e) {
+                EMILoot.LOGGER.error("Critical error encountered while creating all Mob Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }
+        });
+        gameplayFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return ClientLootTables.INSTANCE.gameplayLoots.stream().flatMap(lr -> {
+                    try {
+                        return Stream.of(new GameplayLootRecipe((ClientGameplayLootTable) lr));
+                    } catch (Throwable e) {
+                        EMILoot.LOGGER.error("Critical error encountered while creating a Gameplay loot recipe; recipe skipped: {}", lr.getId().toString());
+                        EMILoot.LOGGER.error("Thrown Error: ", e);
+                        return Stream.of();
+                    }
+                }).toList();
+            } catch (Throwable e) {
+                EMILoot.LOGGER.error("Critical error encountered while creating all Gameplay Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }
+        });
+        archaeologyFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return ClientLootTables.INSTANCE.archaeologyLoots.stream().flatMap(lr -> {
+                    try {
+                        return Stream.of(new ArchaeologyLootRecipe((ClientArchaeologyLootTable) lr));
+                    } catch (Throwable e) {
+                        EMILoot.LOGGER.error("Critical error encountered while creating an Archaeology loot recipe; recipe skipped: {}", lr.getId().toString());
+                        EMILoot.LOGGER.error("Thrown Error: ", e);
+                        return Stream.of();
+                    }
+                }).toList();
+            } catch (Throwable e) {
+                EMILoot.LOGGER.error("Critical error encountered while creating all Archaeology Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }
+        });
+    }
+
     @Override
     public void register(EmiRegistry registry) {
 
@@ -38,29 +140,55 @@ public class EmiClientPlugin implements EmiPlugin {
         registry.addCategory(GAMEPLAY_CATEGORY);
         registry.addCategory(ARCHAEOLOGY_CATEGORY);
 
-        ClientLootTables.INSTANCE.getLoots().forEach(lootReceiver -> {
-            try {
-                if (!lootReceiver.isEmpty()) {
-                    if (lootReceiver instanceof ClientChestLootTable) {
-                        registry.addRecipe(new ChestLootRecipe((ClientChestLootTable) lootReceiver));
-                    }
-                    if (lootReceiver instanceof ClientBlockLootTable) {
-                        registry.addRecipe(new BlockLootRecipe((ClientBlockLootTable) lootReceiver));
-                    }
-                    if (lootReceiver instanceof ClientMobLootTable) {
-                        registry.addRecipe(new MobLootRecipe((ClientMobLootTable) lootReceiver));
-                    }
-                    if (lootReceiver instanceof ClientGameplayLootTable) {
-                        registry.addRecipe(new GameplayLootRecipe((ClientGameplayLootTable) lootReceiver));
-                    }
-                    if (lootReceiver instanceof ClientArchaeologyLootTable) {
-                        registry.addRecipe(new ArchaeologyLootRecipe((ClientArchaeologyLootTable) lootReceiver));
-                    }
-                }
-            } catch (Throwable e) {
-                EMILoot.LOGGER.error("Critical error encountered while registering a loot recipe {}", lootReceiver.getId().toString());
+        if (chestFuture != null) {
+            chestFuture.exceptionally(e -> {
+                EMILoot.LOGGER.error("Critical error encountered while registering all Chest Recipes. Recipes completely skipped!");
                 EMILoot.LOGGER.error("Thrown Error: ", e);
-            }
-        });
+                return new ArrayList<>();
+            }).join().forEach(registry::addRecipe);
+        } else {
+            EMILoot.LOGGER.error("Chest loot recipe future null for an unknown reason; there may have been a problem during EMI initialization");
+            EMILoot.LOGGER.error("Chest recipes skipped!");
+        }
+        if (blockFuture != null) {
+            blockFuture.exceptionally(e -> {
+                EMILoot.LOGGER.error("Critical error encountered while registering all Block Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }).join().forEach(registry::addRecipe);
+        } else {
+            EMILoot.LOGGER.error("Block loot recipe future null for an unknown reason; there may have been a problem during EMI initialization");
+            EMILoot.LOGGER.error("Block recipes skipped!");
+        }
+        if (mobFuture != null) {
+            mobFuture.exceptionally(e -> {
+                EMILoot.LOGGER.error("Critical error encountered while registering all Mob Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }).join().forEach(registry::addRecipe);
+        } else {
+            EMILoot.LOGGER.error("Mob loot recipe future null for an unknown reason; there may have been a problem during EMI initialization");
+            EMILoot.LOGGER.error("Mob recipes skipped!");
+        }
+        if (gameplayFuture != null) {
+            gameplayFuture.exceptionally(e -> {
+                EMILoot.LOGGER.error("Critical error encountered while registering all Gameplay Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }).join().forEach(registry::addRecipe);
+        } else {
+            EMILoot.LOGGER.error("Gameplay loot recipe future null for an unknown reason; there may have been a problem during EMI initialization");
+            EMILoot.LOGGER.error("Gameplay recipes skipped!");
+        }
+        if (archaeologyFuture != null) {
+            archaeologyFuture.exceptionally(e -> {
+                EMILoot.LOGGER.error("Critical error encountered while registering all Archaeology Recipes. Recipes completely skipped!");
+                EMILoot.LOGGER.error("Thrown Error: ", e);
+                return new ArrayList<>();
+            }).join().forEach(registry::addRecipe);
+        } else {
+            EMILoot.LOGGER.error("Archaeology loot recipe future null for an unknown reason; there may have been a problem during EMI initialization");
+            EMILoot.LOGGER.error("Archaeology recipes skipped!");
+        }
     }
 }
