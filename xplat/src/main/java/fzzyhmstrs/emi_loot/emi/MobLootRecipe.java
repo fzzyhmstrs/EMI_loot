a03e81176f8e0f99184f228c9e1dac4f91372ed5
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-11-24 15:28:36 -0500

    0.7.7 finish off set of loot stacks

--- xplat/src/main/java/fzzyhmstrs/emi_loot/emi/MobLootRecipe.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/emi/MobLootRecipe.java
@@ -14,6 +14,7 @@ import fzzyhmstrs.emi_loot.client.ClientMobLootTable;
 import fzzyhmstrs.emi_loot.client.ClientResourceData;
 import fzzyhmstrs.emi_loot.util.*;
 import fzzyhmstrs.emi_loot.util.stack.EntityEmiStack;
+import fzzyhmstrs.emi_loot.util.stack.MobLootEmiStack;
 import me.fzzyhmstrs.fzzy_config.util.FcText;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
@@ -70,6 +71,7 @@ public class MobLootRecipe implements EmiRecipe {
         outputStacks = list;
 
         this.egg = data.spawnEggItem != null ? EmiStack.of(data.spawnEggItem) : null;
+        this.lootStack = new MobLootEmiStack(lootId);
         this.name = data.name;
         this.type = data.entity != null ? data.entity.getType() : null;
     }
@@ -81,6 +83,7 @@ public class MobLootRecipe implements EmiRecipe {
     private final EntityType<?> type;
     @Nullable
     private final EmiStack egg;
+    private final MobLootEmiStack lootStack;
     private final List<WidgetRowBuilder> rowBuilderList = new ArrayList<>();
 
     private void addWidgetBuilders(ClientBuiltPool newPool, boolean recursive) {
