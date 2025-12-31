7bf273346c8cc9cd4596c994962f3befa015244e
SaloEater <danil.lazarev@gmail.com>
2025-12-31 04:13:39 -0600

    cleanup

--- fabric/src/main/java/fzzyhmstrs/emi_loot/fabric/EMILootAgnosFabric.java
+++ fabric/src/main/java/fzzyhmstrs/emi_loot/fabric/EMILootAgnosFabric.java
@@ -57,7 +57,6 @@ public class EMILootAgnosFabric extends EMILootAgnos {
 
     @Override
     protected @Nullable SpawnEggItem getAllSpawnEggsAgnos(EntityType<?> type) {
-
         return null;
     }
 }
