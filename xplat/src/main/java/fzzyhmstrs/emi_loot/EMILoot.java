eb9b9610acf78c7474f2b4062eaa3c71d667485f
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-04 11:23:43 -0500

    0.7.9 reload resources flag

--- xplat/src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
+++ xplat/src/main/java/fzzyhmstrs/emi_loot/EMILoot.java
@@ -135,6 +135,7 @@ public class EMILoot {
         public boolean chestLootAlwaysStackSame = false;
 
         @NonSync
+        @RequiresAction(action = Action.RELOAD_RESOURCES)
         public boolean splitChestLootByTrial = false;
 
         @NonSync
